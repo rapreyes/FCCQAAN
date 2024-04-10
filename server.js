82114dfeb7d734801da348d169902d141fc0e892
rapreyes reyes <reyesraphaeljohn@gmail.com>
2024-04-10 20:06:18 +0000

    ehh

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -13,7 +29,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-res.render('views/pug/index');
+
+  res.render('index', { title: 'Hello', message: 'Please log in' });
+
 });
 
 const PORT = process.env.PORT || 3000;
